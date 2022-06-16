59cd301ab837fee7b4ee20c9d381ed5d834a3c8c
Crisano97 <97keb6@gmail.com>
2022-06-16 17:35:01 +0200

    Bonus 2

--- js/main.js
+++ js/main.js
@@ -12,18 +12,6 @@ const numberWrapperRow = document.getElementById('number-wrapper');
 
 for ( let index = 1; index <= 100; index++){
 
-   
-
-    if ((index % 3) == 0 && (index % 5) == 0) {
-        console.log("FizzBuzz");
-    } else if ((index % 3) == 0) {
-        console.log("Fizz");
-    } else if ((index % 5) == 0) {
-        console.log("Buzz");
-    } else {
-        console.log(index);
-    }
-    
     const flashyBox = document.createElement('div');
 
     flashyBox.classList.add('col-2', 'box');
@@ -31,6 +19,26 @@ for ( let index = 1; index <= 100; index++){
     flashyBox.innerHTML = index;
 
     numberWrapperRow.append(flashyBox);
+
+    if ((index % 3) == 0 && (index % 5) == 0) {
+        console.log("FizzBuzz");
+        flashyBox.classList.add('bg_1');
+        
+    } else if ((index % 3) == 0) {
+        console.log("Fizz");
+        flashyBox.classList.add('bg_2');
+
+    } else if ((index % 5) == 0) {
+        console.log("Buzz");
+        flashyBox.classList.add('bg_3');
+
+    } else {
+        console.log(index);
+        flashyBox.classList.add('bg_4');
+
+    }
+    
+    
 }
 
 
