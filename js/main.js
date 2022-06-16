fd92355e70dbcda9ccab8985c3490ef5308ef9f7
Crisano97 <97keb6@gmail.com>
2022-06-16 16:29:36 +0200

    esercizio completo in console

--- js/main.js
+++ js/main.js
@@ -2,14 +2,21 @@
 // per i multipli di 3 stampi "Fizz" al posto del numero;
 // per i multipli di 5 stampi "Buzz" al posto del numero;
 // per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz";
+let number;
 
 for ( let index = 1; index <= 100; index++){
-    console.log(index);
 
-    if ((index % 3) == 0) {
+    if ((index % 3) == 0 && (index % 5) == 0) {
+        console.log("FizzBuzz");
+    } else if ((index % 3) == 0) {
         console.log("Fizz");
     } else if ((index % 5) == 0) {
         console.log("Buzz");
+    } else {
+        console.log(index);
     }
     
 }
+
+
+
